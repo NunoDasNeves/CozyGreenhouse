053cd70d94fdb6f2df0a696afb26404a8dc2b130
Nuno Das Neves <ndasneves@gmail.com>
2023-11-17 19:56:39 -0800

    add assets, move initial state data and stuff into statedata dir

--- merchants/MerchantData.gd
+++ merchants/MerchantData.gd
@@ -2,4 +2,3 @@ extends Resource
 class_name MerchantData
 
 @export var restock_params: RestockParams
-
--- merchants/RestockItem.gd
+++ merchants/RestockItem.gd
@@ -2,5 +2,5 @@ extends Resource
 class_name RestockItem
 
 @export var item_data: ItemData
-@export var min_num: int
+@export var min_num: int = 10
 #@export var max_num: int
