2f432a1a7421c0158b35b09c37f7cdef89db84ca
Nuno Das Neves <ndasneves@gmail.com>
2023-11-18 10:20:41 -0800

    balance a bit

--- item/components/WaterJugComponent.gd
+++ item/components/WaterJugComponent.gd
@@ -1,9 +1,9 @@
 extends AcquireComponent
 class_name WaterJugComponent
 
-const WATER_JUG_AMOUNT: float = 1
+@export var water_amount: float = 3
 
 func acquire(slot_data: SlotData) -> void:
 	assert(slot_data.item_data.has_component("Acquire"))
-	var amount: float = slot_data.quantity * WATER_JUG_AMOUNT
+	var amount: float = slot_data.quantity * water_amount
 	Global.state.add_water(amount)
